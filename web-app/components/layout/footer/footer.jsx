2d7fb06f9859e27b236c2b9f3918a0b58be21ef6
Tom Walsh <tomwalsh96@gmail.com>
2022-04-10 16:49:56 +0100

    fixed links, now using next/link for routing

--- web-app/components/layout/footer/footer.jsx
+++ web-app/components/layout/footer/footer.jsx
@@ -1,19 +1,19 @@
 import styles from './footer.module.css'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <a
+        <Link
           href="https://tomwalsh96.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
           Built by
           <div className={styles.name}>
             Tom Walsh
           </div>
-        </a>
+        </Link>
       </footer>
     </>
   )
--- web-app/components/layout/navbar/navbar.jsx
+++ web-app/components/layout/navbar/navbar.jsx
@@ -1,29 +1,28 @@
 import styles from './navbar.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Button } from '@chakra-ui/react'
 
 export default function Navbar() {
   return (
     <>
       <div className={styles.navbar}>
-        <a className={styles.logolink}
+        <Link
           href="/"
-          rel="noopener noreferrer"
         >
           <span className={styles.logo}>
             <Image src="/logo-512x512.png" alt="" width={50} height={50} />
           </span>
           <div className={styles.title}>Planti</div>
-        </a>
+        </Link>
         <div className={styles.menu}>
-          <a
+          <Link
             href="/data"
-            rel="noopener noreferrer"
           >
             <Button colorScheme="green" variant="ghost" size="lg">
               Data
             </Button>
-          </a>
+          </Link>
         </div>
       </div>
     </>
